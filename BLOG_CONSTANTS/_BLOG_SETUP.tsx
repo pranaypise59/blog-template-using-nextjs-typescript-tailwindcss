347ce6ac77a1d9ed2601afaec398ceb41da742ec
Pranay Pise <pranaypise59@gmail.com>
2023-01-24 23:57:47 +0530

    blog_setup

--- BLOG_CONSTANTS/_BLOG_SETUP.tsx
+++ BLOG_CONSTANTS/_BLOG_SETUP.tsx
@@ -22,7 +22,22 @@ import { AiFillGithub, AiOutlineTwitter, AiFillLinkedin, AiFillInstagram, AiFill
     ]
 }
  */
-
+export const PRANAY: IAuthor = {
+    name: "Pranay Pise",
+    designation: "Software Developer",
+    bio: "As a Pharmacy background professional, I faced a significant challenge in my life when I discovered my passion for web development. Despite not having any friends in the field, I was determined to learn and grow in this field. I turned to online resources such as YouTube and Google to acquire the necessary knowledge, and even took on freelance work to gain experience and make money. Through persistence and hard work, I was able to land an internship and further hone my skills, proving that with determination and resourcefulness, one can overcome any obstacle and achieve their goals.",
+    profilePic: "",
+    social: [
+        {
+            icon: <AiFillGithub />,
+            link: 'https://github.com/pranaypise59'
+        },
+        {
+            icon: <AiFillLinkedin />,
+            link: 'https://www.linkedin.com/in/pranay-pise-37a797227/'
+        },
+    ]
+}
 export const MAYUR: IAuthor = {
     name: "Mayur Nalwala",
     designation: "Software Engineer",
@@ -59,7 +74,7 @@ export const RUPALI: IAuthor = {
 
 
 // This can your company name / your name etc for SEO purposes
-export const WEBSITE_NAME: string = 'Next Js Blog Template';
+export const WEBSITE_NAME: string = 'Webophihlia';
 export const WEBSITE_URL: string = 'https://nextjs-simple-blog-template.web.app/';
 
 /**
